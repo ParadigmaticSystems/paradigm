0cca642f39f8be3a6ebd9336a122e3b3bf84cbad
Riley <rorih@live.com>
2025-09-17 09:33:11 -0400

    nodes and refs from map

--- lib/graph/node.ex
+++ lib/graph/node.ex
@@ -10,4 +10,49 @@ defmodule Paradigm.Graph.Node do
           owned_by: Paradigm.id() | nil
         }
   defstruct [:id, :class, :data, owned_by: nil]
+
+  def from_map(map) when is_map(map) do
+    %__MODULE__{
+      id: Map.get(map, "id"),
+      class: Map.get(map, "class"),
+      data: parse_data(Map.get(map, "data", %{})),
+      owned_by: Map.get(map, "owned_by")
+    }
+  end
+
+  # Recursively parse data to handle nested refs
+  defp parse_data(data) when is_map(data) do
+    data
+    |> Enum.map(&parse_data_entry/1)
+    |> Enum.into(%{})
+  end
+
+  defp parse_data(data), do: data
+
+  defp parse_data_entry({key, value}) when is_list(value) do
+    {key, Enum.map(value, &parse_ref_or_data/1)}
+  end
+
+  defp parse_data_entry({key, value}), do: {key, parse_ref_or_data(value)}
+
+  defp parse_ref_or_data(%{"id" => id} = map) when is_map(map) and map_size(map) <= 2 do
+    # This looks like a ref if it only has id and optionally composite
+    case Map.get(map, "composite") do
+      composite when composite in [true, false, "true", "false"] ->
+        %Paradigm.Graph.Node.Ref{
+          id: id,
+          composite: parse_boolean(composite)
+        }
+      nil ->
+        %Paradigm.Graph.Node.Ref{id: id}
+      _ ->
+        map
+    end
+  end
+
+  defp parse_ref_or_data(value), do: value
+
+  defp parse_boolean("true"), do: true
+  defp parse_boolean("false"), do: false
+  defp parse_boolean(bool) when is_boolean(bool), do: bool
 end
--- lib/graph/ref.ex
+++ lib/graph/ref.ex
@@ -8,4 +8,15 @@ defmodule Paradigm.Graph.Node.Ref do
   }
 
   defstruct [:id, composite: false]
+
+  def from_map(%{"id" => id} = map) do
+    %__MODULE__{
+      id: id,
+      composite: parse_boolean(Map.get(map, "composite", false))
+    }
+  end
+
+  defp parse_boolean("true"), do: true
+  defp parse_boolean("false"), do: false
+  defp parse_boolean(bool) when is_boolean(bool), do: bool
 end
